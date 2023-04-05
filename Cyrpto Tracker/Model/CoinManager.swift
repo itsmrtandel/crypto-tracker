c4c6cb9898ef40324cbf577c06ed542e1a2942cc
Amit Tandel <46502349+itsmrtandel@users.noreply.github.com>
2023-04-05 16:52:44 -0700

    Add files via upload

---
+++ Cyrpto Tracker/Model/CoinManager.swift
@@ -0,0 +1,65 @@
+//
+//  CoinManager.swift
+//  Cyrpto Tracker
+//
+//  Created by Amit Tandel on 4/5/23.
+//
+
+import Foundation
+
+protocol CoinManagerDelegate {
+    func didUpdatePrice(price: String, currency: String)
+    func didFailWithError(error: Error)
+}
+
+struct CoinManager {
+    
+    var delegate: CoinManagerDelegate?
+    
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "38968CA4-8122-4CAE-9A79-627C9440FC5A"
+        
+    let currencyArray = ["AUD", "USD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","ZAR"]
+    
+    func getCoinPrice(for currency: String) {
+        
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        print(urlString)
+        
+        if let url = URL(string: urlString) {
+            
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    self.delegate?.didFailWithError(error: error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    if let bitcoinPrice = self.parseJSON(safeData) {
+                        let priceString = String(format: "%.2f", bitcoinPrice)
+                        self.delegate?.didUpdatePrice(price: priceString, currency: currency)
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    func parseJSON(_ data: Data) -> Double? {
+        
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(CoinData.self, from: data)
+            let lastPrice = decodedData.rate
+            print(lastPrice)
+            return lastPrice
+            
+        } catch {
+            delegate?.didFailWithError(error: error)
+            return nil
+        }
+    }
+    
+}
+
